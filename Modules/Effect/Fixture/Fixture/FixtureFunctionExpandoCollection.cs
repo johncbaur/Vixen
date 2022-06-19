0fd79baf606c775311d2730a8ae82aab83569a11
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-06-19 14:01:08 -0500

    VIX-3189 Create Intelligent Fixture Strobe Effect
    VIX-3189 Create Intelligent Fixture Strobe Effect

---
+++ Modules/Effect/Fixture/Fixture/FixtureFunctionExpandoCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using VixenModules.Effect.Effect;
+
+namespace VixenModules.Effect.Fixture
+{
+	/// <summary>
+	/// Maintains a collection of fixture function expando objects.
+	/// </summary>
+	public class FixtureFunctionExpandoCollection : NotifyPropertyObservableCollection<IFixtureFunctionExpando>,
+		IDiscoverCollectionItemType
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public FixtureFunctionExpandoCollection() :
+			base("FixtureFunctions")
+		{
+		}
+
+		#endregion
+
+		#region IDiscoverCollectionItemType
+
+		/// <summary>
+		/// Refer to interface documentation.
+		/// </summary>		
+		public Type GetItemType()
+		{
+			// Return the concrete type of the object in the collection
+			return typeof(FixtureFunctionExpando);	
+		}
+
+		#endregion
+	}
+}
